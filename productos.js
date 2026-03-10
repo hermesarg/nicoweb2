fc856328108738e9e96119d0e5c42ae2e2af2b18
hermesarg <44307882+hermesarg@users.noreply.github.com>
2026-03-10 12:25:35 -0300

    Create productos.js

---
+++ productos.js
@@ -0,0 +1,138 @@
+const productos = [
+  {
+    nombre: "Logo Personalizate",
+    categoria: "sticker",
+    imagen: "gallery_remera1.jpg",
+    tag: "Logo",
+    tagClass: "purple",
+    alt: "Logo Personalizate"
+  },
+  {
+    nombre: "Diseño ají",
+    categoria: "sticker",
+    imagen: "gallery_remera2.jpg",
+    tag: "Sticker",
+    tagClass: "cyan",
+    alt: "Diseño ají"
+  },
+  {
+    nombre: "Sticker Servicio Técnico",
+    categoria: "sticker",
+    imagen: "gallery_remera3.jpg",
+    tag: "Sticker",
+    tagClass: "cyan",
+    alt: "Sticker Servicio Técnico"
+  },
+  {
+    nombre: "Bolsas personalizadas",
+    categoria: "otro",
+    imagen: "gallery_remera4.jpg",
+    tag: "Bolsa",
+    tagClass: "orange",
+    alt: "Bolsas personalizadas"
+  },
+  {
+    nombre: "Remera La Chorineta",
+    categoria: "remera",
+    imagen: "gallery_remera5.jpg",
+    tag: "Remera",
+    tagClass: "",
+    alt: "Remera La Chorineta"
+  },
+  {
+    nombre: "Remera D10S",
+    categoria: "remera",
+    imagen: "gallery_remera6.jpg",
+    tag: "Remera",
+    tagClass: "",
+    alt: "Remera D10S"
+  },
+  {
+    nombre: "Remera Carros Food Truck",
+    categoria: "remera",
+    imagen: "gallery_remera7.jpg",
+    tag: "Remera",
+    tagClass: "",
+    alt: "Remera Carros Food Truck"
+  },
+  {
+    nombre: "Pack de gorras",
+    categoria: "gorra",
+    imagen: "gallery_gorra1.jpg",
+    tag: "Gorra",
+    tagClass: "green",
+    alt: "Pack de gorras"
+  },
+  {
+    nombre: "Remera Los Pollos Hermanos",
+    categoria: "remera",
+    imagen: "gallery_gorras_pack.jpg",
+    tag: "Remera",
+    tagClass: "",
+    alt: "Remera Los Pollos Hermanos"
+  },
+  {
+    nombre: "Remera Power Clean",
+    categoria: "remera",
+    imagen: "gallery_gorra2.jpg",
+    tag: "Remera",
+    tagClass: "",
+    alt: "Remera Power Clean"
+  },
+  {
+    nombre: "Gorra Sangre Negra",
+    categoria: "gorra",
+    imagen: "gallery_sticker1.jpg",
+    tag: "Gorra",
+    tagClass: "green",
+    alt: "Gorra Sangre Negra"
+  },
+  {
+    nombre: "Remera Maradona Boca",
+    categoria: "remera",
+    imagen: "gallery_bolsas.jpg",
+    tag: "Remera",
+    tagClass: "",
+    alt: "Remera Maradona Boca"
+  },
+  {
+    nombre: "Gráfica Personalizate",
+    categoria: "otro",
+    imagen: "gallery_pkg1.jpg",
+    tag: "Promo",
+    tagClass: "purple",
+    alt: "Gráfica Personalizate"
+  },
+  {
+    nombre: "Gorra Fuerza Rionegrina",
+    categoria: "gorra",
+    imagen: "gallery_pkg2.jpg",
+    tag: "Gorra",
+    tagClass: "green",
+    alt: "Gorra Fuerza Rionegrina"
+  },
+  {
+    nombre: "Bucket Hat Sangre Negra",
+    categoria: "gorra",
+    imagen: "gallery_gorra_bucket.jpg",
+    tag: "Gorra",
+    tagClass: "green",
+    alt: "Bucket Hat Sangre Negra"
+  },
+  {
+    nombre: "Gorra Trucker INCRO",
+    categoria: "gorra",
+    imagen: "gallery_gorra_trucker2.jpg",
+    tag: "Gorra",
+    tagClass: "green",
+    alt: "Gorra Trucker INCRO"
+  },
+  {
+    nombre: "Pullover Sangre Negra",
+    categoria: "otro",
+    imagen: "gallery_pullover1.jpg",
+    tag: "Pullover",
+    tagClass: "hoodie",
+    alt: "Pullover Sangre Negra"
+  }
+];
